765150e2cf61f00193f94166ac3efe7abfb86cb4
gercho23 <german.gonzalez@humand.co>
2026-07-14 12:06:06 -0300

    Auto: channels.js,

--- channels.js
+++ channels.js
@@ -155,7 +155,7 @@ const channels = [
     customSources: [
       { url: '/dsports-player.html?id=dsports1', type: 'iframe' },
       { url: 'https://otte-qw.live.pv-cdn.net/pdx-nitro/live/clients/dash/enc/3gg2jnixjn/out/v1/e1840e01f3f14563b66bbb944d5cc54c/cenc.mpd', type: 'dash', keyId: 'f8b207c10f3f76aeba32a360ec52b9e4', keyValue: 'afad49d20eb39670e93e371c1d669921' },
-      { url: 'https://stream-xhd.com/channel2/?id=dsports1', type: 'iframe' },
+      { url: 'https://streamx-hd.com/live2.php?stream=dsports', type: 'iframe' },
       { url: 'https://streamhdx.xyz/channel/?id=dsports1', type: 'iframe' },
       { url: 'https://streamtpday1.xyz/global1.php?stream=dsports', type: 'iframe' },
       { url: 'https://sudamericaplay2.com/canal_8112/dsports4541.html', type: 'iframe' },
